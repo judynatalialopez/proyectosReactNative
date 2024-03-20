fa61285378387e42054bf73cb7f2cdc22c1755c6
BOGDFPCSRFOD126\Aprendiz <>
2024-03-20 17:39:17 -0500

    cambios de chef

--- app_react_native/App.tsx
+++ app_react_native/App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar} from 'expo-status-bar';
+import { StyleSheet, Text, View,  Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hola Mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function APP(){
+    return (
+        <view style={styles.container}>
+          <Image
+          source={ require('./assets/chef.jpg')}
+          style={styles.imageBackground}
+          />
+            <view style={styles.form}>
+            </view>
+        </view>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'brack',
+    },
+    imageBackground: {
+        width: '100%',
+        height: '100%',
+        opacity: 0.7,
+        bottom: '30%',
+    },
+    form: {
+        width: '100%',
+        height: '40%',
+        backgroundColor: 'white',
+        position: 'absolute',
+        bottom: 0,
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+
+    }
 });
